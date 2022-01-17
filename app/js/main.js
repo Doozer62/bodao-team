0385effbec865852eca36d74110d1587b4d82b5e
Doozer62 <a.a.kopiev@mail.ru>
2022-01-17 11:17:21 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+   
+$(function() {
+
+
+    $(".footer__menu-list").on("click", "a", function(event) {
+
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+
+        $('body,html').animate({ scrollTop: top }, 1500);
+
+     
+    });
+
+     
+    });
+
+   
